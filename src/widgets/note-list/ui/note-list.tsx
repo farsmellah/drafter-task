837646de7c26dd959e33c9e9b297ebf4f108f6a0
farsmellah <the.real.smartfellah@gmail.com>
2024-07-08 16:55:04 +0300

    style: Update notes grid breakpoints

--- src/widgets/note-list/ui/note-list.tsx
+++ src/widgets/note-list/ui/note-list.tsx
@@ -9,7 +9,7 @@ export default function NoteList({ notesData }: Props) {
     <div className="container text-center">
       <div className="row gy-3 gx-3">
         {notesData.map((note) => (
-          <div className="col-xs-12 cols-sm-6 col-md-4 col-lg-3 " key={note.id}>
+          <div className="col-xs-12 col-md-6 col-lg-4 " key={note.id}>
             <Note
               id={note.id}
               title={note.title}
