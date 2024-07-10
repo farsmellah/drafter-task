54c49f5cd69d6ab93cad833cf6995f89270bf249
farsmellah <the.real.smartfellah@gmail.com>
2024-07-10 22:24:50 +0300

    fix: Add replace buttons with link in view page banner

--- src/_pages/note-view/ui/note-info-banner.tsx
+++ src/_pages/note-view/ui/note-info-banner.tsx
@@ -1,4 +1,5 @@
 import { GetNoteDTO } from "@/src/entities";
+import Link from "next/link";
 
 type Props = {
   noteData: GetNoteDTO;
@@ -9,13 +10,17 @@ export default function NoteInfoBanner({ noteData, isModal }: Props) {
     <div className="container text-start d-flex flex-column align-items-center justify-content-space-between">
       <p className="w-100 text-start">{noteData.content}</p>
 
-      <button className="btn btn-dark w-100">Edit</button>
+      <Link href={`/note/${noteData.id}/edit`} className="btn btn-dark w-100">
+        Edit
+      </Link>
     </div>
   ) : (
     <div className="container mt-5 d-flex flex-column align-items-center justify-content-center">
       <h1>{noteData.title}</h1>
       <p>{noteData.content}</p>
-      <button className="btn btn-dark">Edit</button>
+      <Link href={`/note/${noteData.id}/edit`} className="btn btn-dark">
+        Edit
+      </Link>
     </div>
   );
 }
